bacbdd585b4ca135c5fad20e8da8759293f3fdfc
GeorgeLR87 <jorgelrgarduno@gmail.com>
2022-05-29 18:44:50 -0500

    getServedBuildings finish

--- 1_getServedBuildings.js
+++ 1_getServedBuildings.js
@@ -1,15 +1,36 @@
 function getServedBuildings(buildingCount, routerLocation, routerRange) {
 
-    let serverdBuildings = 0 
-    let provides = []
-    const position = routerLocation.map(p => p)
-    const range = routerRange.map(r => r)
+    let range = []
+    let cleanrange = []    
+    let count = {}
+    let integer = 0
 
-    for (let = i; i <= routerLocation.length;i++) {
-        
-    }
+    for (let i = 0; i < routerLocation.length; i++){
+        for (let j = 0; j < routerRange.length; j++) {
+            if (i === j ) {
+                range.push(routerLocation[j] - routerRange[j], routerLocation[j], routerLocation[j] + routerRange[j])
+            }
+        }
+    }   
     
+    for (let i = 0; i < range.length; i++) {
+        if(range[i] > 0) {
+            cleanrange.push(range[i])
+        }
+    }    
+    
+    cleanrange.forEach(function(num){
+        count[num - 1] = (count[num -1] || 0) + 1
+    })
+
+    for (let i = 0; i < buildingCount.length; i++) {
+        for (const j in count) {
+            if( j == i && buildingCount[i] - count[i]  >= 0 ) {
+            integer = integer + 1
+            }
+        }
+    }  
+    return integer       
 }
 
-
-getServedBuildings([1,2,1,2,2,], [3,1],[1,2])
+console.log(getServedBuildings([1,2,1,2,2], [3,1], [1,2]));
