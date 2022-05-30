fd035aad3b3f0e13457a79531561d7bc3c3ca826
GeorgeLR87 <jorgelrgarduno@gmail.com>
2022-05-29 20:46:12 -0500

    predictDays

--- 2_predictDays.js
+++ 2_predictDays.js
@@ -1,3 +1,17 @@
 function predictDays(day,k) {
     //Write your code here
+    let goodDays = []
+    for (let i = 0; i < day.length; i++) {
+        if(i - k >= 0) {
+            if(day.length - k - i > 0){
+                if(day[i] - day[i + 1] <= 0){
+                    goodDays.push(i + 1)
+                }
+            }
+        }
+    }
+    return goodDays
 }
+
+console.log(predictDays([3,2,2,2,3,4], 2));
+// predictDays([1,0,1,0,1], 1)   
